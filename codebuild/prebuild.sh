9c27eeba782aba3373528cfc19227a5a05855872
ketoiloihp <ketoiloihp@gmail.com>
2019-06-05 15:56:48 +0700

    Update prebuild.sh

--- codebuild/prebuild.sh
+++ codebuild/prebuild.sh
@@ -5,8 +5,7 @@ aws --version
 aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws s3 ls s3://finos-codebuild-cache/
-aws s3 ls s3://finos.mvnrepository/
+aws s3 ls s3://finos.mvnrepository/releases/com/finos/common/1.0.0/
 echo "=== Generating Docker Image Name & Version ==="
 export DOCKER_IMAGE_NAME=$(gradle properties -q | grep "^group:" | awk '{print $2}').$(gradle properties -q | grep "^name:" | awk '{print $2}')
 export DOCKER_IMAGE_VERSION=v$(gradle properties -q | grep "^version:" | awk '{print $2}')
